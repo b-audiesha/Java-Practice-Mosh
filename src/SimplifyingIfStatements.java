d6fb7437c160dc6e33ff2ffbb24089cd14c2a3e0
audiesha brattin <a.brattin@gmail.com>
2021-03-07 16:40:56 -0600

    refactoring and simplifying if statements

---
+++ src/SimplifyingIfStatements.java
@@ -0,0 +1,18 @@
+public class SimplifyingIfStatements {
+  public static void main(String[]args) {
+    int income = 120_000;
+    boolean hasHighIncome = (income > 100_000);
+  }
+  }
+
+  // This is before refactor  -->
+  //    int income = 120_000;
+//    boolean hasHighIncome;
+//    if (income > 100_000)
+//      hasHighIncome = true;
+//      else
+//        hasHighIncome = false;
+//    }
+
+
+
